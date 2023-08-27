a6fcaa2feebcf59ce2fa1a36fd1ec31e692f8286
Gnlow <gnlow@protonmail.com>
2023-08-27 18:35:14 +0900

    init

---
+++ web/src/lib/Map.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Map from "ol/Map"
+    import View from "ol/View"
+    import "../../node_modules/ol/ol.css"
+    import { onMount } from "svelte"
+    
+    onMount(async () => {
+        new Map({
+            target: "map",
+            view: new View({
+                center: [0, 0],
+                zoom: 2,
+            })
+        })
+    })
+</script>
+
+<style>
+    #map {
+        width: 100%;
+        height: 100%;
+    }
+</style>
+
+<div id="map"/>
