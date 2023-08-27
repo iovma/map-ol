888343d1c8225796e7dc6214bc23fc11d0c08a54
Gnlow <gnlow@protonmail.com>
2023-08-27 21:54:23 +0900

    it works

--- web/src/lib/Map.svelte
+++ web/src/lib/Map.svelte
@@ -1,16 +1,35 @@
 <script lang="ts">
-    import Map from "ol/Map"
-    import View from "ol/View"
-    import "../../node_modules/ol/ol.css"
     import { onMount } from "svelte"
+    import "../../node_modules/ol/ol.css"
+    import { Map, View } from "ol"
+    import { Image } from "ol/layer"
+    import { ImageStatic } from "ol/source"
+    import { Projection } from "ol/proj"
+
+    const extent = [0, 0, 3840, 2160]
+    const projection = new Projection({
+        code: "i",
+        units: "pixels",
+        extent,
+    })
     
     onMount(async () => {
         new Map({
             target: "map",
             view: new View({
+                projection,
                 center: [0, 0],
                 zoom: 2,
-            })
+            }),
+            layers: [
+                new Image({
+                    source: new ImageStatic({
+                        url: "https://github.com/zeli-b/sat/blob/2b403307851c333da698c14cfa2a6ef78cbfad11/%EC%84%B8%EA%B3%84%EC%A7%80%EB%8F%84/world.png?raw=true",
+                        projection,
+                        imageExtent: extent,
+                    })
+                })
+            ]
         })
     })
 </script>
